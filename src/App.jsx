c26d6104d6cde2318e8fdbd3065504d93ba5b755
kavin-kumar10 <kavinkumar.prof@gmail.com>
2023-07-01 20:55:48 +0530

    preloader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import React,{useRef} from 'react';
+import React,{useState,useRef} from 'react';
 import './App.scss';
 import Landpage from './Components/Landpage';
 import About from './Components/About';
 import Skills from './Components/Skills';
 import Project from "./Components/Project"
+import Loader from './Components/Loader';
 // import TimeLine from "./Components/TimeLine"
 
 
 function App() {
+  const [loaded,setLoaded] = useState(false);
+  setTimeout(()=>{
+    setLoaded(true);
+  },5000)
   return (
     <div className="App">
-        <Landpage/>
-        <About/>
-        <Skills/>
-        {/* <Project/> */}
-        {/* <TimeLine/> */}
-    </div>
+        {(loaded)?
+        <>
+            <Landpage/>
+            <About/>
+            <Skills/>
+        </>:
+        <Loader/>}
+      </div>
+
   );
 }
 
--- src/Components/Loader/index.jsx
+++ src/Components/Loader/index.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Loader.scss"
 import {skillImg} from "../../Assets/index"
+import gsap from "gsap";
 
 
 const Loader = () =>{
+    useEffect(()=>{
+        gsap.fromTo(".Loader img",1,{scale:0.8},{scale:1,repeat:-1,yoyo:true});
+    })
     return(
     <div className="Loader">
             <img src={skillImg} alt="skill" />
-            <p>Hone your skills</p>
+            <p>Loading ...</p>
     </div>
     )
 }
